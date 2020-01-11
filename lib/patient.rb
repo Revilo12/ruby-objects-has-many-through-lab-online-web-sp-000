db09fd371a6556cc2568e1586a9c326747b49549
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-11 23:41:00 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,3 +1,27 @@
 class Patient
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+
+  def all
+    @@all
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+
+  def doctors
+    Doctor.all.select {|doctor| doctor.patients.include?(patient)}
+  end
 
 end
